d06d2f6d718de7c16ce6032c15b49408f45269ce
Omar Khawaja <khawaja910@gmail.com>
2022-10-15 13:30:56 -0400

    generate mocks

--- proto/hello/generate_proto.sh
+++ proto/hello/generate_proto.sh
@@ -2,3 +2,6 @@
 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./hello.proto
 
+# if you have mockery installed, run the following to generate mocks
+# mockery --all -inpkg
+
